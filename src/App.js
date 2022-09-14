aafe8b411da72a84c9702ab896813b63183e688f
Rafael Tomas <rafaeltomassouza@gmail.com>
2022-09-14 19:53:12 -0300

    feat: add BikeLane

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Box from '@mui/material/Box';
+import Backdrop from '@mui/material/Backdrop';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import LocationCityOutlinedIcon from '@mui/icons-material/LocationCityOutlined';
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
+
 import District from './component/District';
+import BikeLane from './component/BikeLane';;
+
+
+const actions = [
+  { icon: <DirectionsBikeIcon />, name: 'Ciclovia'},
+  { icon: <LocationCityOutlinedIcon />, name: 'Bairros' },
+];
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const [action, setAction] = useState('');
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
-      <District/>
+    <>
+       <SpeedDial
+        ariaLabel="SpeedDial tooltip example"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={e => setAction(action.name)}
+          />
+        ))}
+      </SpeedDial>
+         {action === 'Bairros' || action === ''? <District/>:
+      <BikeLane/>}
+    </>
   );
 }
 
--- src/component/BikeLane.js
+++ src/component/BikeLane.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import Map  from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 import DeckGL, {GeoJsonLayer } from 'deck.gl';
-import mapData from '../data/bairros.json'
+import mapData from '../data/ciclovia.json'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFmYWVsdG9tYXMiLCJhIjoiY2w4MGEybTA5MDAxaTN4bXR3dm5mcXNqcSJ9.LohQcscxtloEgIpbhwzY7g';
 
 const INITIAL_VIEW_STATE = {
   latitude: -8.053473587502072,
   longitude: -34.896249644042669,
-  zoom: 12,
+  zoom: 14,
   bearing: 0,
   pitch: 30
 };
 
-function District() {
+function BikeLane() {
   const [hoverInfo, setHoverInfo] = useState("");
   const arrayMap = mapData.features;
   const layers = [
@@ -29,7 +29,7 @@ function District() {
       pickable: true,
       autoHighlight: true,
       pointType: 'text',
-      lineWidthMinPixels: 1,
+      lineWidthMinPixels: 3,
       onHover: info => setHoverInfo(info),
   })
   ];
@@ -39,8 +39,7 @@ function District() {
       initialViewState={INITIAL_VIEW_STATE}
       controller={true}
       layers={layers}
-      getTooltip={({object}) => object && (object?.properties?.bairro_nome)}
-      >
+      getTooltip={({object}) => object && (object?.properties?.Nome)}>
         <Map
             reuseMaps
             mapStyle="mapbox://styles/mapbox/streets-v11"
@@ -51,4 +50,4 @@ function District() {
   );
 }
 
-export default District;
+export default BikeLane;
